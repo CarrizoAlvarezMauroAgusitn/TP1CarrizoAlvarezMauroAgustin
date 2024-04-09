5d1a914f32fe9f7b7e40f3b4ad8deeeff78dc80a
User <User@DESKTOP-VA9RCLK>
2024-04-09 17:37:42 -0300

    se corrigio para que la entrada del teclado tomé como entero

--- TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio3/EstructuraControlCondicional.java
+++ TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio3/EstructuraControlCondicional.java
@@ -3,21 +3,17 @@ package ar.edu.unju.fi.ejercicio3;
 import java.util.Scanner;
 
 public class EstructuraControlCondicional {
-
-	public static void main(String[] args) {
+asda
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
- Scanner teclado = new Scanner(System.in);	
- System.out.println("ingrese un numero");		
- String numero1 = teclado.next();
-double n = Double.parseDouble(numero1);	
-//int n = numero1;	
- //int numero = 7;
-		//int n = n%2;
-		//System.out.println("resultado: "+numeroFloat);
-		if (n%2=0 ) {
+Scanner teclado = new Scanner(System.in);	
+System.out.println("ingrese un numero");
+int numero;
+numero = teclado.nextInt();	
+		if (numero % 2 == 0 ) {
 		System.out.println("El numero es par");
 	} else {
-		System.out.println("El numero es impar");
+	System.out.println("El numero es impar");
 	}
  
  
