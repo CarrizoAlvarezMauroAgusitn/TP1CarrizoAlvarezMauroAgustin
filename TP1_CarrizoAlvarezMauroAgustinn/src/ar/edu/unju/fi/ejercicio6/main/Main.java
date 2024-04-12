3163c1dc8173367cf6c7b98cd53d0889c8a1a379
User <User@DESKTOP-VA9RCLK>
2024-04-12 19:26:36 -0300

    seguir corrigiendo

--- TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,4 +1,7 @@
 package ar.edu.unju.fi.ejercicio6.main;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import ar.edu.unju.fi.ejercicio6.model.Persona;
 
@@ -7,7 +10,24 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona edad = new Persona ();
-		edad.
-	}
+		Persona persona1 = new Persona();		
+Scanner scanner = new Scanner(System.in);
 
+System.out.print("Ingrese DNI: ");
+int dni = scanner.nextInt();
+System.out.print("Ingrese nombre: ");
+String nombre = scanner.nextLine();
+scanner.nextLine();
+System.out.print("Ingrese Fecha de nacimento (dd/mm/yy): ");
+String fechaNacString = scanner.nextLine();
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/mm/yy");
+LocalDate fechaNac = LocalDate.parse(fechaNacString,formatter);
+System.out.print(formatter.format(fechaNac));
+System.out.print("Ingrese Provincia: ");
+String provincia = scanner.nextLine();
+	
+	//Persona persona1 = new Persona();
+	//persona1.set
+	
+	}
 }
--- TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ TP1_CarrizoAlvarezMauroAgustinn/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -12,7 +12,45 @@ private String provincia;
 public void leerfecha() {
 fechaNa = Integer.parseInt(JOptionPane.showInputDialog("Ingrese fecha de nacimiento: "));
 
+}
 
+//METODOS ACCESORES
+public String getDni() {
+	return dni;
+}
+
+public void setDni(String dni) {
+	this.dni = dni;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public int getFechaNa() {
+	return fechaNa;
+}
+
+public void setFechaNa(int fechaNa) {
+	this.fechaNa = fechaNa;
+}
+
+public String getProvincia() {
+	return provincia;
+}
+
+public void setProvincia(String provincia) {
+	this.provincia = provincia;
+}
+
+
+@Override
+public String toString() {
+	return "Persona [dni=" + dni + ", nombre=" + nombre + ", fechaNa=" + fechaNa + ", provincia=" + provincia + "]";
 }
 	
 }
